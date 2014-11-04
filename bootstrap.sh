abe9b9cef9694bdc72e9aa97df4889c690ce8d9e
Sunny Patel <spatel@leadnomics.com>
2014-11-04 11:33:10 -0500

    Installs Forever globally

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ apt-get install -y build-essential python-software-properties python g++ make
 apt-get update
 apt-get install -y nodejs npm
 ln -s /usr/bin/nodejs /usr/bin/node
-npm install -g sails
+npm install -g sails forever
 cd /home/vagrant/current/
 npm install
 forever start app.js
