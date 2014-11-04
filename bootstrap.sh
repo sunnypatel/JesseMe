26689038908e6f3ad15bf2e723b149360452ecce
Sunny Patel <spatel@leadnomics.com>
2014-11-04 12:03:21 -0500

    GitIngores .vagrant and silences npm install for Vagrant

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ apt-get install -y build-essential python-software-properties python g++ make
 apt-get update
 apt-get install -y nodejs npm
 ln -s /usr/bin/nodejs /usr/bin/node
+npm config set loglevel warn
 npm install -g sails forever
 cd /home/vagrant/current/
 npm install
